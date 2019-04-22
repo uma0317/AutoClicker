414af5b642b33a0ab7e377e94fcdad4e929b19b4
uma0317 <blog3170@gmail.com>
2019-04-23 03:15:18 +0900

    refactoring

--- build/classes/autoclicker/Manage/MainPanel.rs
+++
@@ -1,7 +0,0 @@
-autoclicker.Manage.ConnectRow
-autoclicker.Manage.ListAndInfoPanel
-autoclicker.Manage.TimeRowsPanel
-autoclicker.Manage.TimeRow
-autoclicker.Manage.MainPanel
-autoclicker.Manage.ButtonRow
-autoclicker.Manage.Rows
